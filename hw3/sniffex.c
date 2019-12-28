4b35e55dd328471bc536ed400b9e658bbe215a82
Tim Chen <33782675+timchen88520@users.noreply.github.com>
2019-12-28 19:45:19 +0800

    Add files via upload
    fix -r open file problem, all done

--- hw3/sniffex.c
+++ hw3/sniffex.c
@@ -221,6 +221,13 @@ print_payload(const u_char *payload, int len)
 /*
  * dissect/print packet
  */
+
+char count_ip_srt_name[1000][30];
+int count_ip_srt_name_num[1000];
+int count_ip_srt_num=0;
+char count_ip_dst_name[1000][30];
+int count_ip_dst_name_num[1000];
+int count_ip_dst_num=0;
 	void
 got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
@@ -264,6 +271,39 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	printf("         To: %s\n", inet_ntoa(ip->ip_dst));
 	printf("       Time: %s.%6d\n",timestr,(int)header->ts.tv_usec);
 	printf("     Length: %d bytes\n",header->len);
+
+	char *src,*dst;
+	src=inet_ntoa(ip->ip_src);
+	dst=inet_ntoa(ip->ip_dst);
+
+	int flag1=0,flag2=0;
+
+	for(int i=0;i<count_ip_srt_num;i++)
+		if(strcmp(count_ip_srt_name[i],"src")==0){
+			count_ip_srt_name_num[i]++;
+			flag1=1;
+			break;
+		}
+
+	if(flag1==0){
+		strcpy(count_ip_srt_name[count_ip_srt_num],src);
+		count_ip_srt_num++;
+		count_ip_srt_name_num[count_ip_srt_num]++;
+	}
+
+	for(int i=0;i<count_ip_dst_num;i++)
+		if(strcmp(count_ip_dst_name[i],dst)==0){
+			count_ip_dst_name_num[i]++;
+			flag2=1;
+			break;
+		}
+	if(flag2==0){
+		strcpy(count_ip_dst_name[count_ip_dst_num],dst);
+		count_ip_dst_num++;
+		count_ip_dst_name_num[count_ip_dst_num]++;
+	}
+
+
 	/* determine protocol */	
 	switch(ip->ip_p) {
 		case IPPROTO_TCP:
@@ -316,6 +356,7 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	 */
 	return;
 }
+
 
 int main(int argc, char **argv)
 {
@@ -406,79 +447,93 @@ int main(int argc, char **argv)
 		/* cleanup */
 		pcap_freecode(&fp);
 		pcap_close(handle);
+		printf("Sent from this ip:\n");
+		for(int i=0;i<count_ip_srt_num;i++)
+			printf("%s : %d\n",count_ip_srt_name[i],count_ip_srt_name_num[i]);
+
+		printf("Taken from this ip:\n");
+		for(int i=0;i<count_ip_dst_num;i++)
+			printf("%s : %d\n",count_ip_dst_name[i],count_ip_dst_name_num[i]);
 
 		printf("\nCapture complete.\n");
 
 		return 0;
 	}
 	else{
-			if (argc == 2) {
-	    	filter= argv[1]; 
-	}
-	else if (argc > 2) {
-		fprintf(stderr, "error: unrecognized command-line options\n\n");
-		print_app_usage();
-		exit(EXIT_FAILURE);
-	}
-	else {
-		/* find a capture device if not specified on command-line */
-		dev = pcap_lookupdev(errbuf);
-		if (dev == NULL) {
-			fprintf(stderr, "Couldn't find default device: %s\n",
-			    errbuf);
+		if (argc == 2) {
+			filter= argv[1]; 
+		}
+		else if (argc > 3) {
+			fprintf(stderr, "error: unrecognized command-line options\n\n");
+			print_app_usage();
 			exit(EXIT_FAILURE);
 		}
-	}
-	
-	/* get network number and mask associated with capture device */
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		fprintf(stderr, "Couldn't get netmask for device %s: %s\n",
-		    dev, errbuf);
-		net = 0;
-		mask = 0;
-	}
+		else {
+			/* find a capture device if not specified on command-line */
+			dev = pcap_lookupdev(errbuf);
+			if (dev == NULL) {
+				fprintf(stderr, "Couldn't find default device: %s\n",
+						errbuf);
+				exit(EXIT_FAILURE);
+			}
+		}
 
-	/* print capture info */
-	printf("Device: %s\n", dev);
-	printf("Number of packets: %d\n", num_packets);
-	printf("Filter expression: %s\n", filter);
+		/* get network number and mask associated with capture device */
+		if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+			fprintf(stderr, "Couldn't get netmask for device %s: %s\n",
+					dev, errbuf);
+			net = 0;
+			mask = 0;
+		}
 
-	/* open capture device */
-	handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
-	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-		exit(EXIT_FAILURE);
-	}
+		/* print capture info */
+		printf("Device: %s\n", dev);
+		printf("Number of packets: %d\n", num_packets);
+		printf("Filter expression: %s\n", filter);
 
-	/* make sure we're capturing on an Ethernet device [2] */
-	if (pcap_datalink(handle) != DLT_EN10MB) {
-		fprintf(stderr, "%s is not an Ethernet\n", dev);
-		exit(EXIT_FAILURE);
-	}
+		/* open capture device */
+		handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+		if (handle == NULL) {
+			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+			exit(EXIT_FAILURE);
+		}
 
-	/* compile the filter expression */
-	if (pcap_compile(handle, &fp, filter, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n",
-		    filter, pcap_geterr(handle));
-		exit(EXIT_FAILURE);
-	}
+		/* make sure we're capturing on an Ethernet device [2] */
+		if (pcap_datalink(handle) != DLT_EN10MB) {
+			fprintf(stderr, "%s is not an Ethernet\n", dev);
+			exit(EXIT_FAILURE);
+		}
 
-	/* apply the compiled filter */
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n",
-		    filter, pcap_geterr(handle));
-		exit(EXIT_FAILURE);
-	}
+		/* compile the filter expression */
+		if (pcap_compile(handle, &fp, filter, 0, net) == -1) {
+			fprintf(stderr, "Couldn't parse filter %s: %s\n",
+					filter, pcap_geterr(handle));
+			exit(EXIT_FAILURE);
+		}
 
-	/* now we can set our callback function */
-	pcap_loop(handle, num_packets, got_packet, NULL);
+		/* apply the compiled filter */
+		if (pcap_setfilter(handle, &fp) == -1) {
+			fprintf(stderr, "Couldn't install filter %s: %s\n",
+					filter, pcap_geterr(handle));
+			exit(EXIT_FAILURE);
+		}
 
-	/* cleanup */
-	pcap_freecode(&fp);
-	pcap_close(handle);
+		/* now we can set our callback function */
+		pcap_loop(handle, num_packets, got_packet, NULL);
 
-	printf("\nCapture complete.\n");
+		/* cleanup */
+		pcap_freecode(&fp);
+		pcap_close(handle);
 
-return 0;
+		printf("Sent from this ip:\n");
+		for(int i=0;i<count_ip_srt_num;i++)
+			printf("%s : %d\n",count_ip_srt_name[i],count_ip_srt_name_num[i]);
+
+		printf("Taken from this ip:\n");
+		for(int i=0;i<count_ip_dst_num;i++)
+			printf("%s : %d\n",count_ip_dst_name[i],count_ip_dst_name_num[i]);
+		printf("\nCapture complete.\n");
+
+		return 0;
 	}
 }
